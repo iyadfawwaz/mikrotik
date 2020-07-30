8d73945e253deca8c049b337c9d8533f15a8c94a
إياد الفواز <40076723+iyadseizer@users.noreply.github.com>
2020-07-30 16:19:23 +0300

    Update MainActivity.java

--- app/src/main/java/sy/e/serverconn/MainActivity.java
+++ app/src/main/java/sy/e/serverconn/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    MikrotikServer mserver;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
