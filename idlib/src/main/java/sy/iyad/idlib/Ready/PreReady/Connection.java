71066ab6d272e23a8fdfee80c30cb492e758b11f
إياد الفواز <40076723+iyadseizer@users.noreply.github.com>
2020-08-01 14:13:20 +0300

    mikrotik idlib

--- idlib/src/main/java/sy/iyad/idlib/Ready/PreReady/Connection.java
+++ idlib/src/main/java/sy/iyad/idlib/Ready/PreReady/Connection.java
@@ -6,11 +6,13 @@ import sy.iyad.idlib.Ready.Api;
 import sy.iyad.idlib.Ready.MikrotikApiException;
 
 public class Connection extends AsyncTask<String,Integer, Api> {
+  public static PORTcUstom=8728;
+  public static TIMEOUTcUstom=1000;
     @Override
     protected Api doInBackground(String... strings) {
         Api api;
         try {
-            api = Api.connect(strings[0]);
+            api = Api.connect(SocketFactory.getDefault(),strings[0],PORTcUstom,TIMEOUTcUstom);
             api.login(strings[1],strings[2]);
             return api;
         } catch (MikrotikApiException ignored) {
